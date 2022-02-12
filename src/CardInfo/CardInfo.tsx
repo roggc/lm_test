c83b74e285eba55416c811116fa31ee56a52ffd9
roggc <roggc9@gmail.com>
2022-02-12 19:39:22 +0100

    add vector icons

--- src/CardInfo/CardInfo.tsx
+++ src/CardInfo/CardInfo.tsx
@@ -2,17 +2,26 @@ import React from 'react'
 import styled from 'styled-components/native'
 import {ViewProps, View} from 'react-native'
 import {IHotelData} from '../Master'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 export interface ICardInfoProps extends ViewProps {
   hotelData: IHotelData
 }
 
 export const CardInfo: React.FC<ICardInfoProps> = ({hotelData, ...props}) => {
+  const getStars = () => {
+    const starIcons = []
+    for (let i = 1; i <= hotelData.stars; i++) {
+      starIcons.push(<Icon name="star" solid key={i} />)
+    }
+    return starIcons
+  }
   return (
     <Wrapper {...props}>
       <TitleView>
         <Text>{hotelData.name}</Text>
-        <Text>{hotelData.stars}</Text>
+        <Stars>{getStars()}</Stars>
+        <Text>{hotelData.userRating}</Text>
       </TitleView>
       <Image source={{uri: hotelData.gallery[0]}} />
     </Wrapper>
@@ -29,6 +38,7 @@ const Image = styled.Image`
 `
 const TitleView = styled(View)`
   flex-direction: row;
+  justify-content: space-between;
 `
 
 const Wrapper = styled(View)`
@@ -36,4 +46,9 @@ const Wrapper = styled(View)`
   margin: ${({theme}) => theme.dimensions.margin.simple}px;
   border-radius: ${({theme}) => theme.dimensions.radius}px;
   border: 1px solid ${({theme}) => theme.colors.black};
+`
+
+const Stars = styled(View)`
+  flex-direction: row;
+  align-items: center;
 `
